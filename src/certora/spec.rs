b93a1fda6e44b7cad8ff1b9bc0f6a169794f8fda
Eric Eilebrecht <eric@certora.com>
2024-10-02 13:18:13 -0700

    satisfy

--- src/certora/spec.rs
+++ src/certora/spec.rs
@@ -4,14 +4,14 @@ use crate::DAOContract;
 use crate::types::{
     ballot_category::BallotCategory, ballot_init_params::BallotInitParams, contract_config::ContractConfig
 };
-use cvt::{require, assert};
+use cvt::{require, assert, satisfy};
 use cvt_soroban::{is_auth};
 
 
 #[no_mangle]
 pub fn certora_config_sanity(env: Env, config: ContractConfig) {
     DAOContract::config(env, config);
-    assert!(false);
+    satisfy!(true);
 }
 
 #[no_mangle]
@@ -27,7 +27,7 @@ pub fn certora_config_can_only_be_called_once(env: Env, config: ContractConfig) 
 pub fn certora_create_ballot_sanity(env: Env, params: BallotInitParams) {
     require!(is_auth(params.initiator.clone()), "authorized");
     DAOContract::create_ballot(env, params);
-    assert!(false);
+    satisfy!(true);
 }
 
 #[no_mangle]
@@ -42,7 +42,7 @@ pub fn certora_create_ballot_must_be_initiator(env: Env, params: BallotInitParam
 pub fn certora_ballot_id_increasing(env: Env, params: BallotInitParams) {
     let ballot_id = DAOContract::create_ballot(env.clone(), params.clone());
     let ballot_id2 = DAOContract::create_ballot(env.clone(), params.clone());
-    require!(ballot_id2 > ballot_id, "Ballot ID should be increasing");
+    assert!(ballot_id2 > ballot_id, "Ballot ID should be increasing");
 }
 
 #[no_mangle]
@@ -50,7 +50,7 @@ pub fn certora_retract_ballot_sanity(env: Env, ballot_id: u64) {
     let ballot = DAOContract::get_ballot(env.clone(), ballot_id);
     require!(is_auth(ballot.initiator), "authorized");
     DAOContract::retract_ballot(env, ballot_id);
-    assert!(false);
+    satisfy!(true);
 }
 
 #[no_mangle]
@@ -71,7 +71,7 @@ pub fn certora_retract_ballot_can_only_be_called_once(env: Env, ballot_id: u64) 
 #[no_mangle]
 pub fn certora_vote_sanity(env: Env, ballot_id: u64, accepted: bool) {
     DAOContract::vote(env, ballot_id, accepted);
-    assert!(false);
+    satisfy!(true);
 }
 
 #[no_mangle]
@@ -85,7 +85,7 @@ pub fn certora_cannot_vote_on_retracted_ballot(env: Env, ballot_id: u64, accepte
 pub fn certora_set_deposit_sanity(env: Env, deposit_params: Map<BallotCategory, i128>) {
     require!(is_auth(env.get_admin().unwrap()), "authorized");
     DAOContract::set_deposit(env, deposit_params);
-    assert!(false);
+    satisfy!(true);
 }
 
 #[no_mangle]
